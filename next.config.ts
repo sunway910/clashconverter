150479b67489339d1c45553fb9b3abdf4127091c
sunway910 <shengweizhong910@gmail.com>
2026-01-18 16:06:39 +0800

    feat: support deploy on cf worker

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Disable image optimization (Cloudflare Workers uses static approach)
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default withNextIntl(nextConfig);
